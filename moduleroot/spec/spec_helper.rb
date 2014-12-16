8ca9206db99578bffceefa8d960e07e85af8afb1
Carlos Sanchez <carlos@apache.org>
2014-12-16 10:51:46 +0100

    Add module skeleton

---
+++ moduleroot/spec/spec_helper.rb
@@ -0,0 +1,23 @@
+# This file is managed centrally by modulesync
+#   https://github.com/maestrodev/puppet-modulesync
+
+require 'puppetlabs_spec_helper/module_spec_helper'
+<% (@configs['requires'] || []).each do |r| -%>
+require '<%= r %>'
+<% end -%>
+
+RSpec.configure do |c|
+  c.treat_symbols_as_metadata_keys_with_true_values = true
+
+  c.before(:each) do
+    Puppet::Util::Log.level = :warning
+    Puppet::Util::Log.newdestination(:console)
+  end
+
+end
+
+shared_examples :compile, :compile => true do
+  it { should compile.with_all_deps }
+end
+
+<%= "\n" + @configs['extra_code'] if @configs['extra_code'] -%>
---
+++ moduleroot/spec/spec_helper_acceptance.rb
@@ -0,0 +1,53 @@
+require 'puppet'
+require 'beaker-rspec/spec_helper'
+require 'beaker-rspec/helpers/serverspec'
+
+# overriding puppet installation for the RedHat family distros due to
+# puppet breakage >= 3.5
+def install_puppet(host)
+  host['platform'] =~ /(fedora|el)-(\d+)/
+  if host['platform'] =~ /(fedora|el)-(\d+)/
+    safeversion = '3.4.2'
+    platform = $1
+    relver = $2
+    on host, "rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-#{platform}-#{relver}.noarch.rpm"
+    on host, "yum install -y puppet-#{safeversion}"
+  else
+    super()
+  end
+end
+
+RSpec.configure do |c|
+
+  # Project root
+  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
+  c.before(:each) do
+    Puppet::Util::Log.level = :warning
+    Puppet::Util::Log.newdestination(:console)
+  end
+
+  c.before :suite do
+    hosts.each do |host|
+      unless (ENV['RS_PROVISION'] == 'no' || ENV['BEAKER_provision'] == 'no')
+        begin
+          on host, 'puppet --version'
+        rescue
+          if host.is_pe?
+            install_pe
+          else
+            install_puppet(host)
+          end
+        end
+      end
+
+      # Install module and dependencies
+      puppet_module_install(:source => proj_root, :module_name => proj_root.gsub(/^puppet-/,''))
+
+<% (@configs['modules'] || []).each do |mod| -%>
+      on host, puppet('module', 'install', '<%= mod['name'] %>', '--version=<%= mod['version'] %>'), { :acceptable_exit_codes => [0,1] } <%= "if fact('osfamily') == '#{mod['osfamily']}'" if mod['osfamily']%>
+<% end -%>
+    end
+  end
+
+end
